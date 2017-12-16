521e58748ed3ada66e6025d7db4221802806dea2
Vasco Santos <vasco@vslinux.net>
2017-12-16 18:20:18 +0000

    BTC

--- ESP8266/Arduino_C_Sketchs/BTC.ino
+++ ESP8266/Arduino_C_Sketchs/BTC.ino
@@ -10,6 +10,11 @@ const char* password = "aveiro2017";
 const int httpPort = 443;
 const char* host = "api.coinbase.com";
 const char* url = "/v2/prices/EUR/spot";
+const size_t BUFFER_SIZE = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
+
+char BTCVAL[7] = {};
+char ETHVAL[7] = {};
+char LTCVAL[7] = {};
 
 WiFiClientSecure client;
 
@@ -59,22 +64,35 @@ void GetBTC(){
   }
 
   Serial.print(line);
-               
+
+  DynamicJsonBuffer jsonBuffer(BUFFER_SIZE);
+
+  JsonObject& root = jsonBuffer.parseObject(line);
+
+  if (root.success()) {
+    
+    strcpy(BTCVAL, root["data"][0]["amount"]);
+    strcpy(ETHVAL, root["data"][1]["amount"]);
+    strcpy(LTCVAL, root["data"][2]["amount"]);
+     Serial.println("");
+    Serial.print("BTC: ");
+    Serial.println(BTCVAL);
+       }         
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   unsigned long currentMillis = millis();
   
- if (previousMillis  > 0) {
+
     if (currentMillis - coinpreviousMillis > coininterval) {
 
       coinpreviousMillis = currentMillis;
-
+     Serial.println("A obter...");
      GetBTC();
 
     }
-  }
+  
 
   
 
